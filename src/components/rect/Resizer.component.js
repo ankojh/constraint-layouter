42f330a540089c89cd7bb53d860b981d2d37231a
the-observer-man <ankitkumarojha2@gmail.com>
2019-11-15 23:52:04 +0530

    Add Resizer Handler

--- src/components/rect/Resizer.component.js
+++ src/components/rect/Resizer.component.js
@@ -15,7 +15,7 @@ class Resizer extends Component{
 
 
   resizerUnitMousewDowned(event, resizerType){
-    
+
   }
 
 
@@ -28,15 +28,15 @@ class Resizer extends Component{
       onMouseDown={e=>this.resizerContainerMouseDowned(e)}
       >
 
-      <div className="cl-resizer-unit cl-resizer__left"></div>
-      <div className="cl-resizer-unit cl-resizer__top"></div>
-      <div className="cl-resizer-unit cl-resizer__right"></div>
-      <div className="cl-resizer-unit cl-resizer__bottom"></div>
+      <div className="cl-resizer-unit cl-resizer__left" onMouseDown={e=>this.resizerUnitMousewDowned(e, 'left')}></div>
+      <div className="cl-resizer-unit cl-resizer__top" onMouseDown={e=>this.resizerUnitMousewDowned(e, 'top')}></div>
+      <div className="cl-resizer-unit cl-resizer__right" onMouseDown={e=>this.resizerUnitMousewDowned(e, 'right')}></div>
+      <div className="cl-resizer-unit cl-resizer__bottom" onMouseDown={e=>this.resizerUnitMousewDowned(e, 'bottom')}></div>
 
-      <div className="cl-resizer-unit cl-resizer__top-left"></div>
-      <div className="cl-resizer-unit cl-resizer__top-right"></div>
-      <div className="cl-resizer-unit cl-resizer__bottom-left"></div>
-      <div className="cl-resizer-unit cl-resizer__bottom-right"></div>
+      <div className="cl-resizer-unit cl-resizer__top-left" onMouseDown={e=>this.resizerUnitMousewDowned(e, 'topLeft')}></div>
+      <div className="cl-resizer-unit cl-resizer__top-right" onMouseDown={e=>this.resizerUnitMousewDowned(e, 'topRight')}></div>
+      <div className="cl-resizer-unit cl-resizer__bottom-left" onMouseDown={e=>this.resizerUnitMousewDowned(e, 'bottomLeft')}></div>
+      <div className="cl-resizer-unit cl-resizer__bottom-right" onMouseDown={e=>this.resizerUnitMousewDowned(e, 'bottomRight')}></div>
       
     </div>);
    }
